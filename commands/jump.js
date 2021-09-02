3fc41ca649da9d7b0a1c98806dfb1597dc2ae0cf
YHYeoh <yeohyh-pm18@student.tarc.edu.my>
2021-09-02 18:50:10 +0800

    release jump

--- commands/jump.js
+++ commands/jump.js
@@ -5,22 +5,23 @@ module.exports = {
     description : 'Jumps to specified track',
     options:[
         {
-            name:'number',
-            description:"Where you want to jump to?",
+            name:'tracks',
+            description:"How much tracks to skip?",
             required: true,
             type: 10
+            
         }
     ],
     async execute(interaction, player){
         await interaction.deferReply();
         const queue = player.getQueue(interaction.guildId);
-        const tracksCount = interaction.options.get('number').value;
+        const tracksCount = interaction.options.get('tracks').value;
         if (!queue || !queue.playing)
         return void interaction.followUp({
           content: '❌ | No music is being played!',
         });
         queue.jump(queue.tracks[tracksCount+1]);
 
-        interaction.followUp({ content: `⏭ | Skipped to tracks number ${tracksCount}` });
+        interaction.followUp({ content: `⏭ | Skipped ${tracksCount} tracks!` });
     }
 }
